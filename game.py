eee6395edb9a4334debde283a66a250899c375da
Evieepk12 <eviepk12@gmail.com>
2022-02-22 21:12:23 +0700

    WIP

--- game.py
+++ game.py
@@ -9,4 +9,9 @@ class TicTacToe:
     
     @staticmethod
     def print_board_nums():
-        pass
+        number_board = [[str(i) for i in range (j*3, (j+1)*3)] for j in range(3)]
+        for row in number_board:
+            print('| ' + ' | '.join(row) + ' |')
+
+    def available_moves(self):
+        return [i for i, spot in enumerate(self.board) if spot == ' ']
